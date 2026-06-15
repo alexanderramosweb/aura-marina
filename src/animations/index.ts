86d18d7b2da641bdd8267ad11270f62671501245
alexanderramosweb <alexanderdesarrolloweb@gmail.com>
2026-06-15 09:40:54 -0500

    feat(motion): import GSAP core and establish main entry point architecture

---
+++ src/animations/index.ts
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import { initHero } from "./hero";
+import { initFeatures } from "./features";
+import { initAbout } from "./about";
+import { initAdvantages } from "./advantages";
+import { initPricing } from "./pricing";
+import { initLocation } from "./location";
+import { initContact } from "./contact";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function initAnimations() {
+  initHero();
+
+  initFeatures();
+
+  initAbout();
+
+  initAdvantages();
+
+  initPricing();
+
+  initLocation();
+
+  initContact();
+}
