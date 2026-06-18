90a6c9854e06a1fee25d1a2c63f5ce96c0e91cce
alexanderramosweb <alexanderdesarrolloweb@gmail.com>
2026-06-17 22:48:28 -0500

    feat(motion): integrate Lenis smooth scrolling and sync with page layout

---
+++ src/smooth-scroll/index.ts
@@ -0,0 +1,16 @@
+import Lenis from "lenis";
+
+export function initSmoothScroll() {
+  const lenis = new Lenis({
+    duration: 1.2,
+    smoothWheel: true,
+  });
+
+  function raf(time: number) {
+    lenis.raf(time);
+
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
